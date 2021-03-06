88ae42844966f92e30337949abb9500fe46cce69
mensil <cms-mensil@hotmail.com>
2021-03-06 18:25:30 -0300

    Ajustando a posição para a moscar movimentar

--- Jogo.js
+++ Jogo.js
@@ -1,7 +1,7 @@
 var altura = 0
 var largura = 0
 
-function ajustarTamanhoPalcoJogo(){
+function ajustarTamanhoPalcoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
 
@@ -11,4 +11,22 @@ function ajustarTamanhoPalcoJogo(){
 ajustarTamanhoPalcoJogo()
 
 
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 30
+    var posicaoY = Math.floor(Math.random() * altura) - 30
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoy = posicaoY < 0 ? 0 : posicaoY
+
+    console.log(posicaoX, posicaoY)
+
+    // criando o elemento html
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosquito.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    document.body.appendChild(mosquito)
+}
 
